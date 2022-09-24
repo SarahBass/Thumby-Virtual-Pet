42d48417c05efe769426ecadceafe14492764ea2
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-09-24 12:41:05 -0700

    Update VirtualStarLogic.py

--- LeetCode/VirtualStarLogic.py
+++ LeetCode/VirtualStarLogic.py
@@ -1,10 +1,36 @@
 import random
-pagetype = "sadgas"
+#       _      _               _     ___     _   
+#__   _(_)_ __| |_ _   _  __ _| |   / _ \___| |_ 
+#\ \ / / | '__| __| | | |/ _` | |  / /_)/ _ \ __|
+# \ V /| | |  | |_| |_| | (_| | | / ___/  __/ |_ 
+#  \_/ |_|_|   \__|\__,_|\__,_|_| \/    \___|\__| 
+#
+#Global Variables---------------------
+pageNumber=1                         #
+pagetype = "sadgas"                  #
 userInput=11
-randnum=random.randint(2, 1299)
-randnumber=random.randint(5, 120)
-smallrandom=random.randint(0,10)
-palindromenum=[5,6,7,9,11,12,14,15,17,18,19,21,28,35,36,37,42,63,73,74,84,85,94,101,111,119,126,131,185,202,303,404,409]
+                         #
+randnum=random.randint(2, 1299)      #
+randnumber=random.randint(5, 120)    #
+smallrandom=random.randint(0,10)     #
+palindromenum=[5,6,7,9,11,12,14,15,  #
+               17,18,19,21,28,35,36, #
+               37,42,63,73,74,84,85, #
+               94,101,111,119,126,131,
+               185,202,303,404,409]  #
+                                     #
+gameLibrary = ["Roman Numeral",      #
+               "Palindrome Pal",     #
+               "Binary Beats",       #
+               "Witch's Hex",        #
+               "Power Ball",]        #
+selector = 0                         #  
+#-------------------------------------
+
+
+###########################################################################################
+###################MINI GAME LOGIC ########################################################
+###########################################################################################
 def intToRoman(num: int) -> str:
     M = ["", "M", "MM", "MMM"]
     C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
@@ -27,6 +53,22 @@ def isPalindrome(self, x):
     else:
         return False
 
+class Game:
+    def __init__(self,name, answer, text, userAnswer):
+        self.name = name
+        self.answer = answer
+        self.text = text
+        self.userAnswer= userAnswer
+game1 = Game("Roman Numeral", randnum, intToRoman(randnum),userInput)    
+game2 = Game("Palindrome Pal", isPalindrome(isPalindrome,101), random.choice(palindromenum),isPalindrome(isPalindrome,(userInput*(random.choice(palindromenum))))) 
+game3 = Game("Binary Beats", randnumber,bin(randnumber)[2:], userInput)
+game4 = Game("Witch's Hex",randnumber,hex(randnumber)[2:],userInput)
+game5 = Game("Power Ball",pow(2,smallrandom),smallrandom,userInput)
+
+
+###########################################################################################
+################### BUILD STAR CHARACTER ##################################################
+###########################################################################################
 class Star:
     def __init__(self, food, toy, hyg, sad, sick, happy, angry, edu, level ):
         self.food = food
@@ -41,17 +83,105 @@ class Star:
         
 starpet = Star(0,0,0,0,0,5,0,0,0)
 
-class Game:
-    def __init__(self,name, answer, text, userAnswer):
-        self.name = name
-        self.answer = answer
-        self.text = text
-        self.userAnswer= userAnswer
-game1 = Game("Roman Numeral", randnum, intToRoman(randnum),userInput)    
-game2 = Game("Palindrome Pal", isPalindrome(isPalindrome,101), random.choice(palindromenum),isPalindrome(isPalindrome,(userInput*(random.choice(palindromenum))))) 
-game3 = Game("Binary Beats", randnumber,bin(randnumber)[2:], userInput)
-game4 = Game("Witch's Hex",randnumber,hex(randnumber)[2:],userInput)
-game5 = Game("Power Ball",pow(2,smallrandom),smallrandom,userInput)
+###########################################################################################
+################### MANAGE BACKGROUND CHANGES #############################################
+###########################################################################################
+
+
+def BackgroundSwitch(level,sad, sick, angry,happy)-> str:
+    match pageNumber:
+        case 1:
+            if level == 10:
+                return "gameover"
+            elif level == range(7,9):
+                if sick > 0:
+                    return "starsick"
+                elif angry >0:
+                    return "starangry"
+                elif sad > 0:
+                    return "starsad"
+                else :
+                    if happy > 8:
+                        return "superhappystar"
+                    elif happy == range(7,8):
+                        return "happystar"
+                    elif happy == range(5,6):
+                        return "averagestar"
+                    else:
+                        return "sadstar"
+            elif level ==range(3,6):
+                if sick > 0:
+                    return "nebulasick"
+                elif angry >0:
+                    return "nebulaangry"
+                elif sad > 0:
+                    return "nebulasad"
+                else :
+                    if happy > 8:
+                        return "superhappynebula"
+                    elif happy == range(7,8):
+                        return "happynebula"
+                    elif happy == range(5,6):
+                        return "averagenebula"
+                    else:
+                        return "sadnebula"
+            else :
+                if sick > 0:
+                    return "gassick"
+                elif angry >0:
+                    return "gasangry"
+                elif sad > 0:
+                    return "gassad"
+                else :
+                    if happy > 8:
+                        return "superhappygas"
+                    elif happy == range(7,8):
+                        return "happygas"
+                    elif happy == range(5,6):
+                        return "averagegas"
+                    else:
+                        return "sadgas"
+        case 2:
+            return random.choice(gameLibrary)
+        case 3:
+            match selector:
+                case 1:
+                    return "funfood"
+                case 2:
+                    return "meteor"
+                case 3:
+                    return "shootingstar"
+                case _:
+                    return "Food"
+        case 4:
+            match selector:
+                case 1:
+                    return "animal"
+                case 2:
+                    return "spaceship"
+                case 3:
+                    return "satelite"
+                case _:
+                    return "toy"
+        case 5:
+            match selector:
+                case 1:
+                    return "bath"
+                case 2:
+                    return "brush"
+                case 3:
+                    return "medicine"
+                case _:
+                    return "hyg"
+        case _:
+            return "startscreen"
+#-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=--=x=
+#                   START OF GAME
+#-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=--=x=
+
+#-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=--=x=
+#                   END OF GAME                
+#-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=-=x=--=x=
 print(starpet.food)
 print(game1.answer)
 print(game1.text)
@@ -65,57 +195,4 @@ print(game4.answer)
 print(game5.text)
 print(game5.answer)
 print(starpet.happy)
-
-def mainpage(level,sad, sick, angry,happy):
-    if level == 10:
-        "GameOver"
-    elif level == range(7,9):
-        if sick > 0:
-            "starsick"
-        elif angry >0:
-            "starangry"
-        elif sad > 0:
-            "starsad"
-        else :
-            if happy > 8:
-                "superhappystar"
-            elif happy == range(7,8):
-                "happystar"
-            elif happy == range(5,6):
-                "averagestar"
-            else:
-                "sadstar"
-    elif level ==range(3,6):
-        if sick > 0:
-            "nebulasick"
-        elif angry >0:
-            "nebulaangry"
-        elif sad > 0:
-            "nebulasad"
-        else :
-            if happy > 8:
-                "superhappynebula"
-            elif happy == range(7,8):
-                "happynebula"
-            elif happy == range(5,6):
-                "averagenebula"
-            else:
-                "sadnebula"
-    else :
-        if sick > 0:
-            "gassick"
-        elif angry >0:
-            "gasangry"
-        elif sad > 0:
-            "gassad"
-        else :
-            if happy > 8:
-                "superhappygas"
-            elif happy == range(7,8):
-                "happygas"
-            elif happy == range(5,6):
-                "averagegas"
-            else:
-                "sadgas"
-    
-        
+print(BackgroundSwitch(starpet.level,starpet.sad, starpet.sick, starpet.angry,starpet.happy))
